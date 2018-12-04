3203a2a1660a30300680c4741adb9c35e00eb246
Dmitri Samotessov <dimasamo96@gmail.com>
2018-12-04 09:03:29 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  truth = true
+  i = 0
+  while i < collection.length
+    truth = yield(collection[i])
+    i += 1
+    if truth == false
+      return false
+    end
+  end
+  true
 end
